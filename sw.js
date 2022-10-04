affeda0bf5a65395da8245cbbf4abb12982e97e5
yessygc <yessgarciacampos@gmail.com>
2022-10-04 00:42:03 -0500

    Service Worker - Fetch Event

--- sw.js
+++ sw.js
@@ -1,4 +1,20 @@
 
 
-console.log('Hola mundo 2');
+
+self.addEventListener('fetch', event => {
+
+    if( event.request.url.includes('style.css') ) {
+        event.respondWith( null );
+    } else {
+
+        event.respondWith( fetch( event.request ) );
+        
+    }
+
+
+    
+
+
+});
+
 
